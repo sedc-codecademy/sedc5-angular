daf85ed90f85ca6273eec92048de607ab3b61fad
Wekoslav Stefanovski <swekster@gmail.com>
2017-09-29 19:13:14 +0200

    added input parameters

--- angular-the-second/src/app/app.component.ts
+++ angular-the-second/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  oneFirst = 3;
+  oneSecond = 11;
+  oneOp = "*";
+
+  twoFirst = 2;
+  twoSecond = 3;
+  twoOp = "+";
+
+  threeFirst = 22;
+  threeSecond = 3;
+  threeOp = "-";
+
+  fourFirst = 1;
+  fourSecond = 2;
+  fourOp = "h";
+  
 }
--- angular-the-second/src/components/calculator/calculator.ts
+++ angular-the-second/src/components/calculator/calculator.ts
@@ -1,12 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-calculator",
     template: `<div>
-    I'm a calculator
+    {{ first }} {{ operation }} {{ second }} = {{ result }}
 </div>`
-
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit {
+    @Input() first: number;
+    @Input() second: number;
+    @Input() operation: string;
 
+    result: number;
+
+    ngOnInit() {
+        this.calculate();
+    }
+
+    calculate() {
+        switch (this.operation) {
+            case "+":
+                this.result = this.first + this.second;
+                break;
+            case "-":
+                this.result = this.first - this.second;
+                break;
+            case "*":
+                this.result = this.first * this.second;
+                break;
+            case "/":
+                this.result = Math.floor(this.first / this.second);
+                break;
+            default:
+                this.result = NaN;
+        }
+    }
 }
